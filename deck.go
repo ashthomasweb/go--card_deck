441c273cdd052058b0f2a72104ce9633066d1df3
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-02-15 23:50:30 -0500

    Initial commit, builds deck

---
+++ deck.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// Create a new type called 'deck'
+// which is a slice of stings
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+
+}
